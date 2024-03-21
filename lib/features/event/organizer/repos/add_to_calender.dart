8376da76c33928bb7e6ae5c5cf64048943fcc9f1
amrrhassan <amrhassan98@hotmail.com>
2024-03-21 02:01:04 +0200

    fix string date parsing

--- lib/features/event/organizer/repos/add_to_calender.dart
+++ lib/features/event/organizer/repos/add_to_calender.dart
@@ -4,14 +4,14 @@ DateTime _parseDateTime(String dateTimeText) {
   try {
     // Splitting the text to extract date and time components
     List<String> components = dateTimeText.split(", ");
-    String dateText = components[0]; // "02-08-2024"
-    String timeText = components[1]; // "11:01 PM"
+    String dateText = components[0]; // "27-03-2024"
+    String timeText = components[1]; // "09:00 PM"
 
     // Parsing date
     List<String> dateComponents = dateText.split("-");
     int year = int.parse(dateComponents[2]);
-    int month = int.parse(dateComponents[0]);
-    int day = int.parse(dateComponents[1]);
+    int month = int.parse(dateComponents[1]);
+    int day = int.parse(dateComponents[0]);
 
     // Parsing time
     List<String> timeComponents = timeText.split(":");
@@ -20,7 +20,7 @@ DateTime _parseDateTime(String dateTimeText) {
     String period = timeComponents[1].split(" ")[1];
 
     // Adjusting hour for PM
-    if (period == "PM" && hour < 12) {
+    if (period == "PM" && hour != 12) {
       hour += 12;
     }
 
@@ -31,13 +31,15 @@ DateTime _parseDateTime(String dateTimeText) {
   }
 }
 
+
 Event buildEvent(String date, String location, String eventName, String name, {Recurrence? recurrence}) {
   DateTime startDate = _parseDateTime(date);
   DateTime endDate = startDate.add(const Duration(hours: 2)); // Should be edited
+  print('Start Date is: ${startDate}');
 
   return Event(
     title: eventName,
-    description: '${eventName} | ${name} | Alafein Event',
+    description: '${eventName} | ${name} | AlaFein Event',
     location: location,
     startDate: startDate,
     endDate: endDate,
